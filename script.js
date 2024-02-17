6f8868f957b34f7057ee8c0c1d5de63dbb524abe
Alexis Trejo <alexisemanueltrejo@gmail.com>
2024-02-17 12:31:34 -0300

    Agregando funcion de input para aparecer

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function calcularPromedio() {
-    const grade0 = parseInt(document.getElementById('grade0').value);
-    const grade1 = parseInt(document.getElementById('grade1').value);
-    const grade2 = parseInt(document.getElementById('grade2').value);
-    const grade3 = parseInt(document.getElementById('grade3').value);
-  
-    const promedio = ((grade1 * grade0) + (grade2 * grade0) + (grade3 * grade0));
-    
+function mostrarInput() {
+  var input = document.getElementById("miInput");
+  input.style.display = "block";
+}
 
-    
+function calcularPromedio() {
+  const grade0 = parseInt(document.getElementById('grade0').value);
+  const grade1 = parseInt(document.getElementById('grade1').value);
+  const grade2 = parseInt(document.getElementById('grade2').value);
+  const grade3 = parseInt(document.getElementById('grade3').value);
+  const grade4 = parseInt(document.getElementById('grade4').value);
+
+
+  const promedio = ((grade1 * grade0) + (grade2 * grade0) + (grade3 * grade0) + (grade4  * grade0));
+
+  if (promedio > 0) {
     Swal.fire({
       title: 'Monto a Pagar',
       text: `El Valor Total es :$ ${promedio.toFixed(2)}`,
@@ -15,7 +21,17 @@ function calcularPromedio() {
       confirmButtonText: 'Cerrar'
     });
 
-    //alert(`El Valor Total es $: ${promedio.toFixed(2)}`);
-    //document.getElementById('resultado').innerText = `El Valor total es $: ${promedio.toFixed(2)}`;
-    }
-  
+  } else {
+    Swal.fire({
+      title: 'Error',
+      text: ` Debe ingresar valores Para Hacer Cálculo!`,
+      icon: 'error',
+      confirmButtonText: 'Cerrar'
+    });
+  }
+
+
+
+  //alert(`El Valor Total es $: ${promedio.toFixed(2)}`);
+  //document.getElementById('resultado').innerText = `El Valor total es $: ${promedio.toFixed(2)}`;
+}
